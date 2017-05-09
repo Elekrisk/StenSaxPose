6e14b7097365fb9a0c860140d8e389a848d1e340
Axel Windisch <axel@windisch.nu>
2017-05-09 19:13:11 +0200

    test 2

--- StenSaxPose/Program.cs
+++ StenSaxPose/Program.cs
@@ -10,8 +10,17 @@ namespace StenSaxPose
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello and welcome to Sten Sax Påse! :)");
-            System.Console.ReadKey();
+            Console.WriteLine("Welcome to Sten Sax Påse Online!");
+            Console.WriteLine("If you need help with anything, just write '!h' for contextual help or '!ha' for the full version!");
+            Console.WriteLine("Press any button to continue.");
+            Console.ReadKey();
+
+            WriteWelcomeMessage();
+        }
+
+        static void WriteWelcomeMessage()
+        {
+
         }
     }
 }
