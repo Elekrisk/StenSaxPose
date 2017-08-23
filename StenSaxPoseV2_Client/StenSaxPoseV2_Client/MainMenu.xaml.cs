cea105743cb899c359a2157644f99b234550a7d6
Elekrisk <einar.vilhelm.persson@gmail.com>
2017-08-24 01:17:46 +0200

    started working on a gui version (client), dubbed 'StenSaxPoseV2_Client'

---
+++ StenSaxPoseV2_Client/StenSaxPoseV2_Client/MainMenu.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StenSaxPoseV2_Client
+{
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Page
+    {
+
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void LocalBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SelectLocalGame sl = new SelectLocalGame();
+            NavigationService.Navigate(sl);
+        }
+
+        private void OnlineBtn_Click(object sender, RoutedEventArgs e)
+        {
+            JoinOnlineGame jg = new JoinOnlineGame();
+            NavigationService.Navigate(jg);
+        }
+
+        private void OptionsBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void QuitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ StenSaxPoseV2_Client/StenSaxPoseV2_Client/SelectLocalGame.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StenSaxPoseV2_Client
+{
+    /// <summary>
+    /// Interaction logic for SelectLocalGame.xaml
+    /// </summary>
+    public partial class SelectLocalGame : Page
+    {
+        public SelectLocalGame()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LoadBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //MainMenu mm = new MainMenu();
+            //NavigationService.Navigate(mm);
+
+            NavigationService.GoBack();
+        }
+    }
+}
